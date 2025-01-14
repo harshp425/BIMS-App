499e86748c258b77cfac410bbb12caa47f913d74
Harsh Patel <126726290+harshp425@users.noreply.github.com>
2025-01-14 15:40:59 -0500

    Add files via upload

---
+++ app/api/getAll/users/route.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+
+const jsonFilePath = path.join(process.cwd(), 'users.json');
+
+// Helper function to read the JSON file
+const readJsonFile = () => {
+  try {
+    const rawData = fs.readFileSync(jsonFilePath, 'utf-8');
+    return JSON.parse(rawData);
+  } catch (err) {
+    console.error('Error reading JSON file:', err);
+    return [];
+  }
+};
+
+// API Route Handler for GET requests
+export async function GET(req) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || session.user.title !== 'admin') {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 403 });
+  }
+
+  const users = readJsonFile();
+
+  const results = users;
+
+  return NextResponse.json(results, { status: 200 });
+}
+
+// Handle unsupported HTTP methods
+export async function POST() {
+  return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
+}
